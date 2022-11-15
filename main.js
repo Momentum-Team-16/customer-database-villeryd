1959caea402e9b83e12060820eaa581f88cfc55b
Daniel <danielvillery@gmail.com>
2022-11-15 13:35:33 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,7 @@
+const customerContainer = document.querySelector("#customers");
+for (let customer of customers) {
+    let card = document.createElement("div");
+    card.classList.add("card");
+    card.classList.add("small");
+    let name  = document.createElement("div");
+}
